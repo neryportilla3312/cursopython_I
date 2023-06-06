130c3604c5597cd32cc0c3a416f3dd4aea741faa
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-06-06 14:00:45 -0600

    Primer modelo comentarios
    Se adjunta el archivo sobre el primer modelo climático sencillo, con comentarios sobre las variables e indicando el porque de las constantes.

---
+++ primerModelo.py
@@ -0,0 +1,23 @@
+#comentarios primer modelo
+
+constanteSolar = 1350       #valor de la constante solar 
+
+albedoValor = 0.30          #valor promedio del albedo planetario en la Tierra 
+
+emisividadCuerpoNegro = 1   #valor de la emisividad de cuerpo negro
+# la mayoria de la materia condensada tiene un valor cerecano a 1 
+# toma en cuenta que no existe un cuerpo negro en la realidad, solo en la teoría 
+
+constanteStefan = 5.67e-8   # valor de la constante de Stefan - Boltzmann 
+
+# ten cuidado que el valor de las unidades sea el adecuado 
+
+t = (((constanteSolar)*(1-albedoValor))/(4*emisividadCuerpoNegro*constanteStefan))
+
+# se realiza la división por separado, aunque puedes unir toda la expresión en una sola
+
+t2 = pow(t,1/4)
+
+# se obtiene el valor de la raíz cuarta 
+
+print("El valor de la temperatura es de: " + "{0:.3f}".format(t2)+" K")
