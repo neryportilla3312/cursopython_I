dcca007877c55e654704c4dba739fa322afa7116
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-06-07 23:42:41 -0600

    funciones con parametros
    se agregan funciones con parametros y sin parametros

---
+++ funcionesTipos.py
@@ -0,0 +1,32 @@
+
+#Ejemplo1 : función sin parametros o retorno 
+
+def holaMundo():
+    print("Hola a todo el mundo")
+
+holaMundo() # llamada a la función
+
+#Ejemplo2 : función con parametro 
+
+def holaAlumno(nombre):
+    print("Hola estimado alumno: " + nombre + " ... ")
+
+holaAlumno("Joaquin") # llamada a la función 
+
+# función con multiples parametros 
+
+
+def seq(prec, temp): 
+    temp2 = 2 * temp
+    if prec < temp2
+    return "mes de sequía"
+
+seq(20,12)
+
+
+
+
+
+
+
+
