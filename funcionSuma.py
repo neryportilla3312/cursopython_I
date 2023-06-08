8c1a0e791ad1300e47492690ebe143e988cd80f3
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-06-07 21:59:04 -0600

    se agrega función simple
    se agrega función simple para calcular la media de tres números, el programa tiene limitaciones al agregar mas parametros

---
+++ funcionSuma.py
@@ -0,0 +1,22 @@
+
+def puntuacion(p1, p2, p3): 
+    suma = p1 + p2 + p3
+    return suma / 3
+
+
+media = puntuacion(9,4,7)
+print("La puntuacion de la primera clase es: " + media)
+
+
+
+
+media = puntuacion(9,4,5,6,7)
+print("La puntuacion de la segunda clase es: " + media)
+
+
+
+
+
+
+
+
