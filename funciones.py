2f51220b9b7de02026aa51e93609b0ce0890c3b5
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-06-07 21:55:52 -0600

    se adjunta ejemplos primer parte

---
+++ funciones.py
@@ -0,0 +1,35 @@
+print(min(2,9))
+print(max(6,1))
+print(abs(-89))
+
+print(round(8.1))
+print(round(8.5))
+print(round(8.9))
+
+print(len("12345"))
+
+import os
+
+
+dir(os)
+os.name
+os.listdir()
+
+import random
+
+print(random.randint(1,10))
+
+
+def first_fun(x): 
+    return x*2
+
+
+
+
+
+
+
+
+
+
+
