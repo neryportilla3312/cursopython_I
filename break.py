d16fa648c026ed0473b8b470a615108ca1289d1c
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-08-05 09:22:10 -0600

    Espacios
    modificacion

---
+++ break.py
@@ -0,0 +1,35 @@
+
+# immediately exits whatever loop it is in 
+# skips remaining expressions in code block 
+# exits only innermost loop!
+
+
+#while < condition_1 >: 
+ #   while < condition_2 >
+  #      <expression a> 
+   #     break  
+    #    < expression b>  # se rompe el bucle 
+
+     #   < expression c > 
+
+
+# for 
+
+# know number of iterations 
+# can end early via break 
+#uses a counter 
+#can rewrite a for loop using a while loop
+
+
+# while 
+#unbouned number of iterations 
+#can and early via break 
+#can use a counter but most initialize before loop and increment it inside loop
+# may not be able to rewrite a while loop using a for loop 
+
+
+
+
+
+
+
---
+++ while.py
@@ -0,0 +1,33 @@
+
+
+#while<condition> #evaluates to a boolean  
+ #   <expression>  
+  #  <expression>
+
+
+
+#if <condition> # is true, do all the steps inside the while code block 
+
+#check < condition> again 
+
+
+
+x = 0
+while x < 5: 
+    print(x)
+    x = x+1
+
+
+for x in range (5):
+    print(n)
+
+#for<variable> in range <some num>:
+#<expression>
+#<expression> 
+
+#each time through the loop <variable> takes a value 
+
+#first time <variable> starts at the smallest value 
+
+#next time variable gets the prew value + 1 
+
