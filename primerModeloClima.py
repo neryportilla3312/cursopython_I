043197ae4e5407691db6b15557a719385bb272d3
neryportilla3312 <53507049+neryportilla3312@users.noreply.github.com>
2023-09-01 10:58:45 -0600

    Comentarios
    Se agregan comentarios sobre el codigo, las variables se explican y se limitan los decimales  a un cierto numero de cifras significativas

--- primerModeloClima.py
+++ primerModeloClima.py
@@ -1,14 +1,17 @@
 
 #Primer modelo del clima  
 
-constanteSolar = 1350
-albedoValor = 0.30
-emisividadCuerpoNegro = 1 
-constanteStefan = 5.67e-8
+constanteSolar = 1350 # se establece la constante solar 
+albedoValor = 0.30    # se establece el albedo del planeta Tierra 
+emisividadCuerpoNegro = 1  # se considera un cuerpo negro ideal, se considera el valor de epsilon 
+constanteStefan = 5.67e-8  # se considera a la constante de stefan - boltzmann con notación cientifica 
 
 t = (((constanteSolar)*(1-albedoValor))/(4*emisividadCuerpoNegro*constanteStefan))
+# se debe de tener cuidado con la jerarquia de operaciones 
+# la formula parte del balance de energía 
 
-t2 = pow(t,1/4)
+t2 = pow(t,1/4)    # parte de la formula de T a la cuarta potencia 
+
 
 print("{0:.3f}".format(t2)+" K")
 
